8db2136840139f6bce8c4f9356a6f0f0d8131090
Jônatas Braz <jonatasbsmcarvalho@gmail.com>
2023-10-23 16:58:20 -0300

    .

--- app/Services/Channel/ChannelService.php
+++ app/Services/Channel/ChannelService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services\Channel;
 
+use App\Models\Channel;
 use App\Repositories\Interfaces\ChannelsInterface;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class ChannelService
@@ -14,7 +16,13 @@ class ChannelService
 
     }
 
-    public function formattedChannel($authUser) {
+    public function formattedChannel(Channel $channel) {
+        $channel['partners'] = $channel->users->filter(fn ($user) => $user->id != Auth::user()->id)->values();
+        $channel['preview'] = $channel->messages->isEmpty() ? '': $channel->messages->last();
+        return $channel;
+    }
+
+    public function formattedChannels($authUser) {
         $formattedChannels = $authUser->channels()->with(['users', 'messages'])->get()->map(function ($channel) use ($authUser) {
             $channel->partners = $channel->users->filter(fn ($user) => $user->id != $authUser->id)->values();
             $channel->preview = $channel->messages->isEmpty() ? '': $channel->messages->last();
@@ -24,23 +32,33 @@ class ChannelService
         return $formattedChannels;
     }
 
-    public function createChannel($newChannel) {
+    public function createChannel($data) {
 
-        $users = [$newChannel['user1'], $newChannel['user2']];
-        $category = ['category_id' => $newChannel['category_id']];
+        $users = [
+            $data['auth_id'],
+            $data['partner_id']
+        ];
 
+        $channel = [
+            'icon' => $data['icon'],
+            'category_id' => $data['category_id']
+        ];
 
         DB::beginTransaction();
         try {
-            $filledChannel = $this->channelsRepository->fill($category);
-            $filledChannel->save();
+
+
+            $filledChannel = Channel::create($channel);
             $filledChannel->users()->sync($users);
+
             DB::commit();
 
         } catch (QueryException $exception) {
             DB::rollback();
             throw new \Exception($exception);
         }
+
+        return $filledChannel;
 
     }
 }
